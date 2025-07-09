2b21561ee2d628a67f61d0bfe61a148202c045df
anirudhVASUDEV23 <anirudh.vasudev.kesana@gmail.com>
2025-07-09 18:16:18 +0530

    Added convex queries and mutations

---
+++ convex/todos.ts
@@ -0,0 +1,78 @@
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+//if we are just getting the data we use queries
+export const getTodos = query({
+  handler: async (ctx) => {
+    const todos = await ctx.db.query("todos").order("desc").collect();
+    return todos;
+  },
+});
+
+//if we are adding,updating,deleting we use mutations
+export const addTodo = mutation({
+  args: {
+    text: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const todoId = await ctx.db.insert("todos", {
+      text: args.text,
+      isCompleted: false,
+    });
+    return todoId;
+  },
+});
+
+//toggle todo
+export const toggleTodo = mutation({
+  args: { id: v.id("todos") },
+  handler: async (ctx, args) => {
+    const todo = await ctx.db.get(args.id);
+    if (!todo) {
+      throw new ConvexError("Todo not found");
+    }
+    await ctx.db.patch(args.id, {
+      isCompleted: !todo.isCompleted,
+    });
+  },
+});
+
+//update the todo
+export const updateTodo = mutation({
+  args: { id: v.id("todos"), text: v.string() },
+  handler: async (ctx, args) => {
+    const todo = await ctx.db.get(args.id);
+    if (!todo) {
+      throw new ConvexError("Todo not found");
+    }
+    await ctx.db.patch(args.id, {
+      text: args.text,
+    });
+  },
+});
+
+//delete todo mutation
+export const deleteTodo = mutation({
+  args: { id: v.id("todos") },
+  handler: async (ctx, args) => {
+    const todo = await ctx.db.get(args.id);
+    if (!todo) {
+      throw new ConvexError("Todo not found");
+    }
+    await ctx.db.delete(args.id);
+  },
+});
+
+//delete all the todos
+export const clearAllTodos = mutation({
+  handler: async (ctx) => {
+    const todos = await ctx.db.query("todos").collect();
+
+    //Delete all todos
+    for (const todo of todos) {
+      await ctx.db.delete(todo._id);
+    }
+
+    return { deletedCount: todos.length };
+  },
+});
